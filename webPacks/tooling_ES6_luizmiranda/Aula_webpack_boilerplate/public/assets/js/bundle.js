d178584852b94d6b35f3441fab16fe4f5eea43cc
luk3rF7 <lkzeron@gmail.com>
2023-06-10 15:50:11 -0300

    Pasta sobre webpack / babel e config

---
+++ webPacks/tooling_ES6_luizmiranda/Aula_webpack_boilerplate/public/assets/js/bundle.js
@@ -0,0 +1,2 @@
+console.log("Eiiiiiiiiii");
+//# sourceMappingURL=bundle.js.map
---
+++ webPacks/tooling_ES6_luizmiranda/Aula_webpack_boilerplate/src/index.js
@@ -0,0 +1,4 @@
+import './assets/css/style.css';
+import executa from './module/Aula_core_regenerator_runtime'
+
+executa()
