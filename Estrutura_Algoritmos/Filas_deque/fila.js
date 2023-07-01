48bbc59899fb65632ba3ee028fcbbcd206b0a776
luk3rF7 <lkzeron@gmail.com>
2023-07-01 17:59:18 -0300

    Update fila.js
    deque em fila

--- Estrutura_Algoritmos/Filas_deque/fila.js
+++ Estrutura_Algoritmos/Filas_deque/fila.js
@@ -1,4 +1,4 @@
-class fila {
+class Fila {
   constructor() {
     //manter controle
     this.count = 0;
@@ -21,4 +21,43 @@ class fila {
     this.items[this.count] = elem;
     this.count++;
   }
+  dequeue() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return result;
+  }
+  isEmpty() {
+    return this.size() === 0;
+  }
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.lowestCount];
+  }
+  size() {
+    return this.count - this.lowestCount;
+  }
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return "";
+    }
+    let objString = `${this.items[this.lowestCount]}`;
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`;
+    }
+    return objString;
+  }
 }
+const fila = new Fila();
+
+module.exports = fila;
