5203903c462cb722b7974e115d81c3aa23d88894
Lucas Felipe <lkzeron@gmail.com>
2023-08-09 17:37:02 -0300

    feat: componetização base

---
+++ HelloWord/08_componetizacao/Readme.md
@@ -0,0 +1,10 @@
+<h1> Sobre Componentização  </h1>
+
+
+<p>Em um projeto sem framework ou libs utilizamos : </p>
+
+<li> app-components -> responsavel pelos components especificos da aplicaççao como renderizar o grafico
+<li> base-components -> pasta onde fica components de base do projeto exemplo: div,h4,tabela etc
+<li>Domain -> e onde fica elemento dos meu dominios onde tem regras de negocios onde agrego mes,anos,dispesas etc
+<li>views -> onde fica responsavel visual da API 
+<li> e temos main onde e responsavel por renderizar aplicação
---
+++ HelloWord/08_componetizacao/app_components/grafico.js
@@ -0,0 +1,26 @@
+class Grafico {
+  constructor() {
+    this.element = document.createElement("div");
+    this.element.className = "grafico";
+    this.cores = ["red", "yellow", "green", "blue"];
+  }
+
+  adicionarColuna(valor, descricao) {
+    /* criando coluna  */
+    const coluna = document.createElement('div');
+    coluna.className = "grafico-coluna";
+    /*  adicionando cor */
+    const cor = document.createElement('div');
+    /* definindo tamanho e cor */
+    cor.style.height = (valor * 100) / 10000;
+    cor.style.background = this.cores.pop();
+    coluna.appendChild(cor);
+
+    const nomeDoMes = document.createElement("div");
+    nomeDoMes.className = "grafico-coluna-texto";
+    nomeDoMes.innerText = descricao;
+    coluna.appendChild(cor);
+    coluna.appendChild(nomeDoMes);
+    this.element.appendChild(coluna);
+  }
+}
