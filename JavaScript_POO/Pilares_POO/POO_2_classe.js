a024ae8151137da340834d2d351b878f2863187e
Luk3rf7 <lkzeron@gmail.com>
2023-09-27 23:57:12 -0300

    feat: como funciona classe

---
+++ JavaScript_POO/Pilares_POO/POO_2_classe.js
@@ -0,0 +1,50 @@
+class Pessoa {
+  imprimir(name, age) {
+    this.name = name;
+    this.age = age;
+
+    return `O nome da pessoa é: ${this.name} 
+    sua idade é: ${this.age}`
+  }
+
+  imc(peso, altura) {
+    this.peso = peso;
+    this.altura = altura;
+
+    return "imc: " + this.peso / (this.altura * this.altura);
+  }
+}
+
+const pessoa = new Pessoa();
+
+console.log(pessoa.imprimir('user', 18))
+console.log(pessoa.imc(82.5, 1.80))
+
+class Caixa {
+  depoistar(valor) {
+    this.saldo += valor;
+
+    return `Depositou um valor de R$ ${valor}`
+  }
+
+  mostrarSaldo() {
+    return `Saldo atual R$ ${this.saldo}`
+  }
+
+  retirarSaldo(retirar) {
+    return this.saldo -= '-', retirar
+  }
+
+}
+
+const caixa = new Caixa();
+caixa.saldo = 100;
+console.log(caixa.mostrarSaldo())
+console.log(caixa.depoistar(100))
+console.log(caixa.depoistar(100))
+console.log(caixa.depoistar(100))
+console.log(caixa.depoistar(100))
+console.log(caixa.mostrarSaldo())
+console.log('Sacou ======')
+console.log(caixa.retirarSaldo(200))
+console.log(caixa.mostrarSaldo())
